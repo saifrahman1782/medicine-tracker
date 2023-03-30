9f2c099497ac77da0bbea1e15c3051e015df73f2
saifrahman1782 <saifrahman1782@outlook.com>
2023-03-30 19:02:28 +0100

    calendar changes again

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarComponent } from 'ionic2-calendar';
-import { CalendarMode, Step } from 'ionic2-calendar';
+import { CalendarMode, QueryMode, Step } from 'ionic2-calendar';
 
 @Component({
   selector: 'app-home',
